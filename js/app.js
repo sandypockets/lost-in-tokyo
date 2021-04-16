9204e3391e384d79b1c9d435fc2dc5bd8a555a46
sandypockets <zac@znacreative.com>
2021-04-16 17:05:34 -0400

    Add highlight component and colors

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
+// Highlight words
+const Highlight = props => (
+  <span className={`relative highlight highlight-${props.color}`}>
+    <span className={`relative z-2`}>{props.children}</span>
+  </span>
+)
+
 // Intro text
 const Intro = () => (
   <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
     {/* a div with a margin-bottom (mb3 and mb4 on non-small screens) */}
     <div className="mb3 mb4-ns">
-      Lost in Tokyo is a directory of fun places to see, play in and explore, in Tokyo, Japan.
+      <Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color='yellow'>explore</Highlight>, in Tokyo, Japan.
     </div>
     <div>
-      From museums and galleries, to robot restaurants and kitten cafes, Tokyo is the gift that
-      keeps on giving. Dattebayo!
+      From <Highlight color='blue'>museums</Highlight> and <Highlight color='blue'>galleries</Highlight>, to <Highlight color='pink'>robot restaurants</Highlight> and <Highlight color='pink'>kitten cafes</Highlight>, <Highlight color='blue'>Tokyo</Highlight> is the gift that
+      keeps on giving. <Highlight color='yellow'>Dattebayo!</Highlight>
     </div>
   </div>
 )

ace6292bc520cfa6632ee65acb32f356917a48e9
sandypockets <zac@znacreative.com>
2021-04-16 21:18:50 -0400

    Tidy comments

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ const Highlight = ({ color, children }) => (
 // Intro text
 const Intro = () => (
   <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
-    {/* a div with a margin-bottom (mb3 and mb4 on non-small screens) */}
     <div className="mb3 mb4-ns">
       <Highlight color='aqua'>Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color='yellow'>explore</Highlight>, in Tokyo, Japan.
     </div>
@@ -33,7 +32,6 @@ const Nav = () => (
   <nav className='pt3 pt4-ns mb3 mb0-ns'>
     <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0'>
       {menu.map(item =>
-      // The spread operator is the same as: children={item.children} href={item.href} className={item.className}
         <NavItem {...item} />
       )}
     </ul>
@@ -61,22 +59,6 @@ class Attraction extends React.Component {
     )
   }
 }
-
-// const Attraction = ({title, description, className, image}) => (
-//   <div
-//     className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
-//   >
-//     <div className="relative">
-//       <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
-//         <div>
-//           <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
-//           <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
-//         </div>
-//       </div>
-//       <img src={`../images/${image}`} className="db" />
-//     </div>
-//   </div>
-// )
 
 // Main app component
 const App = () => (

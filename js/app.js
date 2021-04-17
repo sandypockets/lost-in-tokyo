5defa493d748796bdd2c7d4557b4d59fcfc6fea0
sandypockets <zac@znacreative.com>
2021-04-16 20:13:17 -0400

    Set up Nav component

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+
 // Highlight words
-const Highlight = props => (
-  <span className={`relative highlight highlight-${props.color}`}>
-    <span className={`relative z-2`}>{props.children}</span>
+const Highlight = ({ color, children }) => (
+  <span className={`relative highlight highlight-${color}`}>
+    <span className={`relative z-2`}>{children}</span>
   </span>
 )
 
@@ -19,11 +20,32 @@ const Intro = () => (
   </div>
 )
 
+const NavItem = ({ className, href, children }) => (
+  <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+    <a className='white no-underline' href={href}>
+      {children}
+    </a>
+  </li>
+
+)
+
+const Nav = () => (
+  <nav className='pt3 pt4-ns mb3 mb0-ns'>
+    <ul className='list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0'>
+      {menu.map(item =>
+      // The spread operator is the same as: children={item.children} href={item.href} className={item.className}
+        <NavItem {...item} />
+      )}
+    </ul>
+  </nav>
+)
+
 // Main app component
 const App = () => (
   <div>
     <div className="min-vh-100 ph4 flex flex-column">
-      {/* our navigation component */}
+      {/* Nav */}
+      <Nav />
       <Intro />
     </div>
     <div className="flex flex-wrap container">{/* our attractions list component */}</div>

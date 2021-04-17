1ade5b224a7b86510297d0e45450cbe1856edfab
sandypockets <zac@znacreative.com>
2021-04-16 20:57:05 -0400

    Add styling to Attraction component

--- js/app.js
+++ js/app.js
@@ -40,11 +40,19 @@ const Nav = () => (
   </nav>
 )
 
-const Attraction = ({title, description, image, className}) => (
-  <div className={className}>
-    <h1>{title}</h1>
-    <p>{description}</p>
-    <img src={`../images/${image}`}/>
+const Attraction = ({title, description, className, image}) => (
+  <div
+    className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
+  >
+    <div className="relative">
+      <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
+        <div>
+          <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+          <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
+        </div>
+      </div>
+      <img src={`../images/${image}`} className="db" />
+    </div>
   </div>
 )
 
